12705b88a280fb2b6491b0123a5e6ce2ba128748
Chris Field <chrisfield22@gmail.com>
2020-03-07 20:20:13 -0500

    Added plots.

--- MarshElevChange_main.R
+++ MarshElevChange_main.R
@@ -65,6 +65,7 @@ MERI_sets <- cbind(MERI_sets, region)
 MACWA_sets <- rbind(MACWA_sets, DENERR_sets, EBF_sets, MERI_sets)
 
 # subtract the minimum year from the year records so that each is the number of years since the first SET measurement
+# mute this when using a baseline of zero for each location instead of separate intercepts
 #MACWA_sets$year <- MACWA_sets$year - min(MACWA_sets$year)
 
 # get a list of the unique site names
@@ -72,6 +73,7 @@ sites_num <- unique(MACWA_sets$site)
 
 ###THIS IS PROBABLY WHERE YOU GET RID OF THE FIRST YEARS###
 # go through and standardize years to a patch specific first date, then get rid of all of the rows with the lowest date; loop through site num
+# mute this for loop and command when using separate intercepts for locations instead of standardizing to zero
 starting_year <- rep(0, length(MACWA_sets$year))
 MACWA_sets <- cbind(MACWA_sets, starting_year)
 for(i in 1:length(sites_num)){
@@ -117,6 +119,10 @@ for(i in 1:length(geo_unique)){
   sets_site_list$geo1[sets_site_list$geo1==geo_unique[i]] <- i
 }
 
+# export list of sites used in the final analysis for map
+sets_site_out <- sets_site_list[, c("site", "lat", "long")]
+write.csv(sets_site_out, "/Users/chrisfield/Dropbox/USFWScontract/accretion_data/final_data/SETs_post_analysis.csv")
+
 # create vectors for Stan 
 # index for site
 site_index <- as.numeric(MACWA_sets$site)
@@ -128,12 +134,14 @@ patches_index <- mat.or.vec(length(MACWA_sets$pin_ht), 1)
 restrict <- mat.or.vec(length(MACWA_sets$pin_ht), 1)
 # index for marsh vegetation type (1 = low marsh)
 veg <- mat.or.vec(length(MACWA_sets$pin_ht), 1)
+slt <- mat.or.vec(length(MACWA_sets$pin_ht), 1)
 # expand vectors so that they are the length of the data vector, instead of the length of the number of sites (NN vs N in Stan)
 for(i in 1:length(MACWA_sets$pin_ht)){
   geo_index[i] <- as.numeric(sets_site_list$geo1[site_index[i]])
   patches_index[i] <- as.numeric(sets_site_list$PatchID[site_index[i]])
   restrict[i] <- as.numeric(sets_site_list$Restric_ha[site_index[i]])/(as.numeric(sets_site_list$area_ha[site_index[i]]))
   veg[i] <- as.numeric(sets_site_list$Veg_cover[site_index[i]])
+  slt[i] <- as.numeric(sets_site_list$noaa_slt[site_index[i]])
 }
 
 # if there is a NA for restrict, assume there is no restriction
@@ -167,9 +175,9 @@ library(rstan)
 rstan_options(auto_write = TRUE)
 setwd("/Users/chrisfield/Documents/folders/GitProjects/MarshElevChange/")
 #setwd("/Users/chrisfield/Desktop/")
-#input_data <- list(data_vec = data_vec, year_index = year_index, site_index=site_index, N=N, NN=NN, region_index=region_index)
-input_data <- list(data_vec = data_vec, year_index = year_index, N=N, NN=NN, NNN=NNN, geo_index=geo_index, patches_index=patches_index, site_index=site_index, restrict=restrict, veg=veg)
-fit_cp <- stan(file='MEC_model_test_real_data_full.stan', data=input_data, iter=20000, warmup=10000, chains=1, seed=483892929, refresh=1200, control = list(adapt_delta = 0.99))
+input_data <- list(data_vec = data_vec, year_index = year_index, N=N, NN=NN, NNN=NNN, geo_index=geo_index, patches_index=patches_index, site_index=site_index, restrict=restrict, veg=veg, slt=slt)
+#input_data <- list(data_vec = data_vec, year_index = year_index, N=N, NNN=NNN, geo_index=geo_index, patches_index=patches_index, restrict=restrict, veg=veg)
+fit_cp <- stan(file='MEC_model_test_real_data_full_relative.stan', data=input_data, iter=20000, warmup=10000, chains=1, seed=483892929, refresh=1200, control = list(adapt_delta = 0.99))
 fit_cp <- stan(file='MEC_model_test_real_data.stan', data=input_data, iter=20000, warmup=10000, chains=1, seed=400, refresh=1200, control = list(adapt_delta = 0.99))
 print(fit_cp)
 params_cp <- as.data.frame(extract(fit_cp, permuted=FALSE))
@@ -192,52 +200,135 @@ for(i in 1:length(patches_unique)){
 geo_pred[i] <- as.numeric(sets_site_list$geo1[sets_site_list$PatchID==i][1])
 slr[i] <- as.numeric(sets_site_list$noaa_slt[sets_site_list$PatchID==i][1])
 forest[i] <- as.numeric(sets_site_list$Loss_prop[sets_site_list$PatchID==i][1])
-preds[, i] <- params_cp$`chain:1.D` + params_cp[,paste("chain:1.geo[", geo_pred[i], "]", sep="")] + params_cp$`chain:1.B`*0 + params_cp$`chain:1.C`*0 + params_cp[,paste("chain:1.patch[", i, "]", sep="")]
+preds[, i] <- params_cp$`chain:1.D` + params_cp[,paste("chain:1.geo[", geo_pred[i], "]", sep="")] + params_cp$`chain:1.B`*0 + params_cp$`chain:1.C`*0 + params_cp[,paste("chain:1.patch[", i, "]", sep="")] + params_cp$`chain:1.E`*slr[i]
 preds_relative[, i] <- preds[, i] - slr[i]
 }
 
+slt_x <- seq(1.5, 6, by=0.05)
+slt_line <- mat.or.vec(length(params_cp$`chain:1.D`), length(slt_x))
+upper <- mat.or.vec(length(slt_x), 1)
+lower <- mat.or.vec(length(slt_x), 1)
+for(i in 1:length(slt_x)){
+slt_line[, i] <- params_cp$`chain:1.D` + params_cp$`chain:1.E`*slt_x[i]
+upper[i] <- quantile(slt_line[, i], c(0.975))
+lower[i] <- quantile(slt_line[, i], c(0.025))
+}
+
+
 # find the posterior distribution for the proportion of patches that are keeping pace with SLR
 prop_above0 <- mat.or.vec(length(params_cp$`chain:1.D`), 1)
+forest_corr <- mat.or.vec(length(params_cp$`chain:1.D`), 1)
 # for loop to cycle through each step of the MCMC chain
 for(i in 1:length(params_cp$`chain:1.D`)){
   # subtract the slr rate for each column (patch) for each step of the MCMC chain
   prop_above0[i] <- length(which((preds[i, ] - slr) > 0))/length(patches_unique)
+  forest_corr[i] <- cor(forest, preds[i, ])
 }
 
-quartz.options(width=6, height=4)
+# multi-panel plot to compare MEC with forest loss and slr
+quartz.options(width=5.14, height=4)
 layout(matrix(c(1, 2, 3, 4), 2, 2, byrow = TRUE))
 
 par(mar=c(3, 3, 1, 1))
-plot(slr, colMeans(preds), ylim=c(-8, 10), xlim=c(2, 5), pch=16, col=rgb(0, 0, 0, 0.8), cex.axis=0.75, xlab=" ", ylab=" ")
-abline(coef = c(0,1))
-for(i in 1:length(slr)){
-segments(slr[i], quantile(preds[,i], c(.025)), slr[i], quantile(preds[,i], c(.975)), lend="butt", lwd=10, rgb(0, 0, 0, 0.1))
-}
-mtext(side=2, line=2, "Marsh ele. change (mm/year)", cex=0.75)
-mtext(side=1, line=2, "Sea level rise (mm/year)", cex=0.75)
-
-par(mar=c(3, 3, 1, 1))
-hist(prop_above0, cex.axis=0.75, main=" ")
-mtext(side=1, line=2, "Prop. of sites above SLR rate", cex=0.75)
-
-par(mar=c(3, 3, 1, 1))
-plot(forest, colMeans(preds_relative), ylim=c(-11, 8), pch=16, col=rgb(0, 0, 0, 0.8), cex.axis=0.75)
+plot(forest, colMeans(preds_relative), ylim=c(-11, 9), xlim=c(-0.005, 0.055), pch=16, col=rgb(0, 0, 0, 0.8), cex.axis=0.75, mgp=c(3, 0.5, 0))
 for(i in 1:length(slr)){
   segments(forest[i], quantile(preds_relative[,i], c(.025)), forest[i], quantile(preds_relative[,i], c(.975)), lend="butt", lwd=10, rgb(0, 0, 0, 0.1))
 }
-mtext(side=2, line=2, "Marsh ele. change (mm/year)", cex=0.75)
-mtext(side=1, line=2, "Forest loss (% of patch)", cex=0.75)
+mtext(side=2, line=1.75, "Marsh ele. change (mm/year)", cex=0.65)
+mtext(side=1, line=1.5, "Forest loss (prop. of patch)", cex=0.65)
+abline(lm(colMeans(preds_relative) ~ forest))
+
+par(mar=c(3, 1, 1, 1))
+hist(forest_corr, cex.axis=0.75, main=" ", yaxt="n", mgp=c(3, 0.5, 0))
+mtext(side=1, line=1.5, "Correlation between forest loss and MEC", cex=0.65)
+
+par(mar=c(3, 3, 1, 1))
+plot(slr, colMeans(preds), ylim=c(-8, 12), xlim=c(2.5, 4.55), pch=16, col=rgb(0, 0, 0, 0.8), cex.axis=0.75, xlab=" ", ylab=" ", mgp=c(3, 0.5, 0))
+abline(coef = c(0,1), lwd=2)
+for(i in 1:length(slr)){
+segments(slr[i], quantile(preds[,i], c(.025)), slr[i], quantile(preds[,i], c(.975)), lend="butt", lwd=10, rgb(0, 0, 0, 0.1))
+}
+mtext(side=2, line=1.75, "Marsh ele. change (mm/year)", cex=0.65)
+mtext(side=1, line=1.5, "Sea level rise (mm/year)", cex=0.65)
+#lines(slt_x, lower, lwd=2, col=rgb(0, 0, 0, 0.5))
+#lines(slt_x, upper, lwd=2, col=rgb(0, 0, 0, 0.5))
+#lines(slt_x, colMeans(slt_line), lwd=2, col=rgb(0, 0, 0, 0.5))
+abline(lm(colMeans(preds)~slr), col=rgb(0, 0, 0, 0.5), lwd=2)
+conf_int <- predict(lm(colMeans(preds)~slr), newdata=data.frame(slr=slt_x), interval="confidence", level = 0.95)
+lines(slt_x, conf_int[,2], col=rgb(0, 0, 0, 0.5), lwd=1)
+lines(slt_x, conf_int[,3], col=rgb(0, 0, 0, 0.5), lwd=1)
+
+par(mar=c(3, 1, 1, 1))
+hist(prop_above0, cex.axis=0.75, main=" ", yaxt="n", mgp=c(3, 0.5, 0))
+mtext(side=1, line=1.5, "Prop. of sites above SLR rate", cex=0.65)
 
 
-#########EXTRA CODE###########
-hist(params_cp$`chain:1.aslope_mu`, main=" ", xlab="mm/year")
-slope_means <- mat.or.vec(max(site_index), 1)
+# plot to demonstrate variation at multiple levels in a hierarchical model
+# create a matrix of posterior predictions for site-level trends, with a row for each step of the MCMC chain
+site_means <- mat.or.vec(length(params_cp$`chain:1.D`), max(site_index))
 for(i in 1:max(site_index)){
-  slope_means[i] <- mean(params_cp[,paste("chain:1.slope[", i, "]", sep="")])
-  points(slope_means[i], 0, pch=16, col=rgb(0, 0, 0, 0.4))
+  site_means[, i] <- mean(params_cp[,paste("chain:1.site[", i, "]", sep="")] + params_cp$`chain:1.D` + params_cp$`chain:1.E`*mean(slt))
 }
 
+# create a vector of the mean posterior predictions for patch-level trends
+patch_means <- mat.or.vec(max(patches_index), 1)
+for(i in 1:max(patches_index)){
+  patch_means[i] <- mean(params_cp[,paste("chain:1.patch[", i, "]", sep="")] + params_cp$`chain:1.D` + params_cp$`chain:1.E`*mean(slt))
+}
 
+# create a vector of the mean posterior prediction for geo-morphic setting level trends
+geo_means <- mat.or.vec(max(geo_index), 1)
+for(i in 1:max(geo_index)){
+  geo_means[i] <- mean(params_cp[,paste("chain:1.geo[", i, "]", sep="")] + params_cp$`chain:1.D` + params_cp$`chain:1.E`*mean(slt))
+}
 
+quartz.options(width=5.14, height=4)
+par(mar=c(3, 3, 1, 1))
+plot(colMeans(site_means), rep(0, max(site_index)), ylim=c(0, 3.1), pch=16, col=rgb(0, 0, 0, 0), cex.axis=0.75, xlab=" ", ylab=" ", bty="n", yaxt="n", mgp=c(3, 0.5, 0))
+
+# add a vertical line for the mean SLR rate in the region
+abline(v=mean(unique(slt)), lwd=2, col="cadetblue")
+
+abline(h=0, col=rgb(0, 0, 0, 0.5))
+abline(h=1, col=rgb(0, 0, 0, 0.5))
+abline(h=2, col=rgb(0, 0, 0, 0.5))
+abline(h=3, col=rgb(0, 0, 0, 0.5))
+
+# choose which Patch ID to use as an example for the figure
+ex <- 1
+
+# plot the mean and 95% prediction intervals (using mean estimates of the SD) for the sites within a patch
+segments(patch_means[ex], 0, patch_means[ex], 1, col="red", lwd=2)
+polygon(x=c((patch_means[ex] - mean(params_cp$`chain:1.site_stdev`)*1.98), (patch_means[ex] - mean(params_cp$`chain:1.site_stdev`)*1.98), (patch_means[ex] + mean(params_cp$`chain:1.site_stdev`)*1.98), (patch_means[ex] + mean(params_cp$`chain:1.site_stdev`)*1.98)),
+        y=c(0, 1, 1, 0), border=NA, col=rgb(1, 0, 0, 0.2))
+
+# plot the mean and 95% prediction intervals (using mean estimates of the SD) for the patches within a geomorphic setting
+segments(geo_means[as.numeric(sets_site_list$geo1[sets_site_list$PatchID==ex][1])], 1, geo_means[as.numeric(sets_site_list$geo1[sets_site_list$PatchID==ex][1])], 2, col="red", lwd=2)
+geoplot <- geo_means[as.numeric(sets_site_list$geo1[sets_site_list$PatchID==ex][1])]
+polygon(x=c((geoplot - mean(params_cp$`chain:1.patch_stdev`)*1.98), (geoplot - mean(params_cp$`chain:1.patch_stdev`)*1.98), (geoplot + mean(params_cp$`chain:1.patch_stdev`)*1.98), (geoplot + mean(params_cp$`chain:1.patch_stdev`)*1.98)),
+        y=c(1, 2, 2, 1), border=NA, col=rgb(1, 0, 0, 0.2))
+
+# plot the mean and 95% prediction intervals (using mean estimates of the SD) for the variation in geomorphic setting from the overall regional mean
+hypermean <- mean(params_cp$`chain:1.D` + params_cp$`chain:1.E`*mean(slt))
+segments(hypermean, 2, hypermean, 3, col="red", lwd=2)
+polygon(x=c((hypermean - median(params_cp$`chain:1.geo_stdev`)*1.98), (hypermean - median(params_cp$`chain:1.geo_stdev`)*1.98), (hypermean + median(params_cp$`chain:1.geo_stdev`)*1.98), (hypermean + median(params_cp$`chain:1.geo_stdev`)*1.98)),
+        y=c(2, 3, 3, 2), border=NA, col=rgb(1, 0, 0, 0.2))
+
+# plot the posterior means for site, patch, and geomorphic setting-level predictions
+points(patch_means, rep(1, max(patches_index)), col=rgb(0, 0, 0, 0.8), pch=16)
+points(geo_means, rep(2, max(geo_index)), col=rgb(0, 0, 0, 0.8), pch=16)
+points(mean(params_cp$`chain:1.D` + params_cp$`chain:1.E`*mean(slt)), 3, col="red", pch=16)
+points(colMeans(site_means), rep(0, max(site_index)), col=rgb(0, 0, 0, 0.8), pch=16)
+
+# add red markers to the sites, patches, and geomoprhic setting associated with the example (ex)
+points(colMeans(site_means[ ,sets_site_list$PatchID==ex]), rep(0, length(colMeans(site_means[ ,sets_site_list$PatchID==ex]))), col="red", pch=16)
+points(patch_means[ex], 1, col="red", pch=16)
+points(geo_means[as.numeric(sets_site_list$geo1[sets_site_list$PatchID==ex][1])], 2, col="red", pch=16)
+
+mtext(line=1.5, side=1, "Marsh elevation change (mm/year)", cex=0.75)
+text(-7.75, 0.1, "SET locations", pos=4, cex=0.75)
+text(-7.75, 1.1, "Marsh patches", pos=4, cex=0.75)
+text(-7.75, 2.1, "Geomorphic setting", pos=4, cex=0.75)
+text(-7.75, 3.1, "Regional mean", pos=4, cex=0.75)
 
 
